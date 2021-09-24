5d4669058f30ee6ddda2b94806a90fb29c9416a7
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-09-24 02:38:46 -0400

    update

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 
 const express = require('express');
 
+const conectarDB = require('./config/db')
+
 // create the server
 
 const app = express();
 
+//conect DB
+conectarDB()
 // port of the app
 const PORT = process.env.PORT || 4000;
 
