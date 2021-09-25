e5391c64f7121f355ac3263612e2126fcb6626c0
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-09-25 00:07:51 -0400

    checking that the user has not been previously registered

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,16 @@
 const User = require('../models/User')
 
 exports.creatUser = async (req, res) =>{
+
+    const { email, password } = req.body
+
     try {
-        let user;
+        //check that the registered user is unique
+
+        let user= await User.findOne({email});
+        if (user){
+            return res.status(400).json({msg: 'el usuario ya existe'})
+        }
         // creat user new
         user = new User(req.body)
 
