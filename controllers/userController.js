f0fa3e714f0cf7ed0af5b484c8d95c78ce1ab83e
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-09-25 00:51:42 -0400

    adding validation API

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,19 @@
 const User = require('../models/User');
 const bcryptjs = require('bcryptjs');
+const {validationResult} = require('express-validator');
 
 exports.creatUser = async (req, res) =>{
 
     const { email, password } = req.body
 
     try {
+        //check for errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()})
+        }
+
+
         //check that the registered user is unique
 
         let user= await User.findOne({email});
