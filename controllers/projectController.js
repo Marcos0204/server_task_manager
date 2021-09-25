3fd30c7a41509f7d4b3e96bb3a3a8ff2b634cdfe
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-09-25 12:28:28 -0400

    insert-new-project

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,4 +1,14 @@
+const Project = require('../models/Project')
 
 exports.createProject = async (req, res) =>{
-    console.log('create projecto')
+    try {
+        // create new project
+        const project = new Project(req.body)
+        project.save();
+        res.json(project);
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error')
+    }
 }
