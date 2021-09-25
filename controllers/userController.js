be4c558e42c3fb68f0ba8284f34cd62762038a78
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-09-24 23:56:58 -0400

    insert user in the DB

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,18 @@
+const User = require('../models/User')
 
+exports.creatUser = async (req, res) =>{
+    try {
+        let user;
+        // creat user new
+        user = new User(req.body)
 
-exports.creatUser = (req, res) =>{
-    console.log(req.body)
+        // add user
+        await user.save()
+
+        // confir
+        res.send('usuario creado correctamente')
+    } catch (error) {
+        console.log(error)
+        res.status(400).send('hubo un error')
+    }
 }
